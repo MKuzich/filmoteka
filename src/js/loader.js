875db4b88bb9f7d6cc869a3f536301e40c88c2e8
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-16 20:53:49 +0300

    done preloader

---
+++ src/js/loader.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    
+window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 2600);
+}
