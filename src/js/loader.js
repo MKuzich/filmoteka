3c713ede435a43015b266774b7067d0c5ba18048
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-17 21:04:34 +0300

    preloader changed

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,8 +1,9 @@
+
 window.onload = function () {
     document.body.classList.add('loaded_hiding');
     
 window.setTimeout(function () {
       document.body.classList.add('loaded');
       document.body.classList.remove('loaded_hiding');
-    }, 2600);
+    }, 500);
 }
