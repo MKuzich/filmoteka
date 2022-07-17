0f09ee31cb79c6bbc70c591c7ef78cc56b58c61c
Mykhailo Kuzich <timmkedth@gmail.com>
2022-07-17 20:37:43 +0300

    fixed url for imeges render

--- src/js/api/api-service-trending.js
+++ src/js/api/api-service-trending.js
@@ -6,11 +6,6 @@ const MEDIA_TYPE = 'movie';
 const TIME_WINDOW = 'day';
 const SEARCH_URL = `${BASE_URL}/trending/${MEDIA_TYPE}/${TIME_WINDOW}`;
 
-
-    export function fetchTrendingMovies() {
-         return axios.get(
-            `${SEARCH_URL}?api_key=${API_KEY}`,
-          );
-      }
-
-
+export function fetchTrendingMovies() {
+  return axios.get(`${SEARCH_URL}?api_key=${API_KEY}`);
+}
--- src/js/trending-markup.js
+++ src/js/trending-markup.js
@@ -1,19 +1,21 @@
 import { fetchTrendingMovies } from './api/api-service-trending';
 
-const listFilms = document.querySelector('.list-films')
+const listFilms = document.querySelector('.list-films');
 
-trendingMarkup()
+trendingMarkup();
 
 function trendingMarkup() {
-    fetchTrendingMovies()
-        .then(({data}) => {
-            console.log(data);
-            const markup = data.results.map(el => 
+  fetchTrendingMovies()
+    .then(({ data }) => {
+      console.log(data);
+      const markup = data.results
+        .map(
+          el =>
             `<li class="link list-films_card">
             <a class="link list-films_card-info">
                 <img
                     class="list-films_card-info_card-film"
-                    src=${el.poster_path}
+                    src=https://image.tmdb.org/t/p/original/${el.poster_path}
                     alt="movie poster"
                     width="395"
                     height="574"
@@ -27,12 +29,12 @@ function trendingMarkup() {
                 </div>
             </a>
             </li>`
-            ).join('');
-        
-            listFilms.insertAdjacentHTML('afterbegin', markup);
-        
-        })
-        .catch ((error) => {
-        console.log(error)
+        )
+        .join('');
+
+      listFilms.insertAdjacentHTML('afterbegin', markup);
     })
-};
+    .catch(error => {
+      console.log(error);
+    });
+}
