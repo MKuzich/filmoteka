6ec36d1fcb78f3d9d9fc29f841ddc151bfe67fb8
Mykhailo Kuzich <timmkedth@gmail.com>
2022-07-17 22:35:10 +0300

    fixed type=password in forms, added close on bckdrp click

--- src/js/signup-modal.js
+++ src/js/signup-modal.js
@@ -7,6 +7,12 @@ const refs = {
 refs.openModalBtn.addEventListener('click', openModal);
 refs.closeModalBtn.addEventListener('click', closeSignupModal);
 
+function closeSignupModalOnAreaClick(e) {
+  if (e.target.classList.contains('backdrop')) {
+    closeSignupModal();
+  }
+}
+
 function closeModalOnEsc(e) {
   e.key === 'Escape' ? closeSignupModal() : null;
 }
@@ -15,10 +21,12 @@ export function closeSignupModal() {
   document.body.classList.remove('modal-open');
   refs.modal.classList.add('is-hidden');
   document.removeEventListener('keydown', closeModalOnEsc);
+  refs.modal.removeEventListener('click', closeSignupModalOnAreaClick);
 }
 
 function openModal() {
   document.body.classList.add('modal-open');
   refs.modal.classList.remove('is-hidden');
   document.addEventListener('keydown', closeModalOnEsc);
+  refs.modal.addEventListener('click', closeSignupModalOnAreaClick);
 }
