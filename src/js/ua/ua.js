6e10fe030eaab7abaed4a877bcb16bd332c53334
orestznak <orestznak@gmail.com>
2022-07-19 00:11:20 +0300

    headerUa

--- src/js/ua/ua.js
+++ src/js/ua/ua.js
@@ -1,12 +1,13 @@
 
 const lang = document.querySelector("#checkbox");
-const logoText = document.querySelector('.logo__text');
+// const logoText = document.querySelector('.logo__text');
 const menuHome = document.querySelector('#home');
 const menuLibrary = document.querySelector('#library');
 const searchInput = document.querySelector('.search-form-input');
 const btnLogIn = document.querySelector('[data-login-modal-open]');
 const btnSignUp = document.querySelector('[data-signup-modal-open]');
  
+
 // console.log(btnLogIn);
 
 lang.addEventListener('input', onChange);
@@ -25,7 +26,7 @@ function onChange ()  {
 
 
 function enVersion() {
-  logoText.textContent = 'Filmoteka';
+  // logoText.textContent = 'Filmoteka';
   menuHome.textContent = 'Home';
   menuLibrary.textContent = 'My Library';
   searchInput.setAttribute('placeholder',"Movie search");
@@ -34,7 +35,7 @@ function enVersion() {
 }
 
 function uaVersion() {
-  logoText.textContent = 'Фільмотека';
+  // logoText.textContent = 'Фільмотека';
   menuHome.textContent = 'Домашня';
   menuLibrary.textContent = 'Моя бібліотека';
   searchInput.setAttribute('placeholder',"Пошук фільмів");
