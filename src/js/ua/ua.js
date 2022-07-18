865d1dad5b8f0a7616374eb288a80aa7355d45ac
orestznak <orestznak@gmail.com>
2022-07-19 00:08:45 +0300

    headerUa

--- src/js/ua/ua.js
+++ src/js/ua/ua.js
@@ -6,30 +6,16 @@ const menuLibrary = document.querySelector('#library');
 const searchInput = document.querySelector('.search-form-input');
 const btnLogIn = document.querySelector('[data-login-modal-open]');
 const btnSignUp = document.querySelector('[data-signup-modal-open]');
-
-
-
-
+ 
 // console.log(btnLogIn);
 
 lang.addEventListener('input', onChange);
 
 function onChange ()  {
     if(!lang.checked) {
-        logoText.textContent = 'Filmoteka';
-        menuHome.textContent = 'Home';
-        menuLibrary.textContent = 'My Library';
-        searchInput.setAttribute('placeholder',"Movie search");
-        btnLogIn.textContent = 'Log in';
-        btnSignUp.textContent = 'Sign up';
-
+        enVersion();
     } else {
-        logoText.textContent = 'Фільмотека';
-        menuHome.textContent = 'Домашня';
-        menuLibrary.textContent = 'Моя бібліотека';
-        searchInput.setAttribute('placeholder',"Пошук фільмів");
-        btnLogIn.textContent = 'Вхід';
-        btnSignUp.textContent = 'Реєстрація';
+        uaVersion();
 
     };
 
@@ -37,22 +23,21 @@ function onChange ()  {
 
 }
 
-function enMenu() {
-    return `<li class="menu__item">
-    <a href="/src/index.html" class="menu__link current">Home</a>
-  </li>
-  <li class="menu__item">
-    <a href="/src/library.html" class="menu__link">My library</a>
-  </li>`;
 
+function enVersion() {
+  logoText.textContent = 'Filmoteka';
+  menuHome.textContent = 'Home';
+  menuLibrary.textContent = 'My Library';
+  searchInput.setAttribute('placeholder',"Movie search");
+  btnLogIn.textContent = 'Log in';
+  btnSignUp.textContent = 'Sign up';
 }
 
-function uaMenu() {
-    return `<li class="menu__item">
-    <a href="/src/index.html" class="menu__link current">Домашня</a>
-  </li>
-  <li class="menu__item">
-    <a href="/src/library.html" class="menu__link">Моя бібліотека</a>
-  </li>`;
-
+function uaVersion() {
+  logoText.textContent = 'Фільмотека';
+  menuHome.textContent = 'Домашня';
+  menuLibrary.textContent = 'Моя бібліотека';
+  searchInput.setAttribute('placeholder',"Пошук фільмів");
+  btnLogIn.textContent = 'Вхід';
+  btnSignUp.textContent = 'Реєстрація';
 }
