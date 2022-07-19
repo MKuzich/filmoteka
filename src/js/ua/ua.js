f743c7518d2267c6b26c17de37f8c577ee2be683
orestznak <orestznak@gmail.com>
2022-07-19 12:00:36 +0300

    headerUa+lang
    При зміні мови ломається навігація в хедері (зникаються самі посилання та псевдоелементи)
    При зміні на українську мову потрібно також додавати атрибут lang="uk", при зміні на англійську - атрибут видаляти

--- src/js/ua/ua.js
+++ src/js/ua/ua.js
@@ -1,6 +1,6 @@
 
-const lang = document.querySelector("#checkbox");
-// const logoText = document.querySelector('.logo__text');
+const selectedLang = document.querySelector("#checkbox");
+const languege = document.querySelector('html');
 const menuHome = document.querySelector('#home');
 const menuLibrary = document.querySelector('#library');
 const searchInput = document.querySelector('.search-form-input');
@@ -8,27 +8,37 @@ const btnLogIn = document.querySelector('[data-login-modal-open]');
 const btnSignUp = document.querySelector('[data-signup-modal-open]');
  
 
-// console.log(btnLogIn);
+console.log(localStorage.getItem('localLang'));
 
-lang.addEventListener('input', onChange);
+if(localStorage.getItem('localLang')) {
+  selectedLang.checked = true;
+  uaVersion()
+} else {
+  selectedLang.checked = false;
+  languege.setAttribute('html', 'en');
+  enVersion();
+}
 
-function onChange ()  {
-    if(!lang.checked) {
+
+selectedLang.addEventListener('input', onChange);
+
+function onChange (evt)  {
+  evt.preventDefault();
+    if(!selectedLang.checked) {
         enVersion();
     } else {
         uaVersion();
-
     };
-
-    //console.log(lang.checked);
-
 }
 
 
 function enVersion() {
-  // logoText.textContent = 'Filmoteka';
-  menuHome.textContent = 'Home';
-  menuLibrary.textContent = 'My Library';
+  
+  languege.setAttribute('html', 'en');
+  localLang();
+
+  menuHome.innerHTML = '<a href="index.html" class="menu__link current">Home</a>';
+  menuLibrary.innerHTML = '<a href="library.html" class="menu__link">My library</a>';
   searchInput.setAttribute('placeholder',"Movie search");
   btnLogIn.textContent = 'Log in';
   btnSignUp.textContent = 'Sign up';
@@ -36,9 +46,19 @@ function enVersion() {
 
 function uaVersion() {
   // logoText.textContent = 'Фільмотека';
-  menuHome.textContent = 'Домашня';
-  menuLibrary.textContent = 'Моя бібліотека';
+  languege.setAttribute('html', 'uk');
+  console.log('pislia '+languege.getAttribute('html'));
+  localLang();
+
+
+  menuHome.innerHTML = '<a href="index.html" class="menu__link current">Домашня</a>';
+  menuLibrary.innerHTML = '<a href="library.html" class="menu__link">Моя бібліотека</a>';
   searchInput.setAttribute('placeholder',"Пошук фільмів");
   btnLogIn.textContent = 'Вхід';
   btnSignUp.textContent = 'Реєстрація';
+}
+
+
+function localLang() {
+  localStorage.setItem('localLang',languege.getAttribute('html'));
 }
