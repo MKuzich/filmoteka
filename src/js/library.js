7251f930497c9204af7d0c46da24f7a1649dab47
Mykhailo Kuzich <timmkedth@gmail.com>
2022-07-19 23:49:29 +0300

    added main logic for library

--- src/js/library.js
+++ src/js/library.js
@@ -1,33 +1,53 @@
-import { authStatus } from './auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { moviesGenresConvertation } from './movies-genres-convertation';
 
-// userLoginCheck(USER_ID);
-// authStatus().then(uid => {
-//   return console.log(uid);
-// });
+let USER_ID = null;
 
-function userLoginCheck(uid) {
-  if (uid) {
-    console.log('loginned');
-    markupLibraryRender(uid);
+const auth = getAuth();
+onAuthStateChanged(auth, user => {
+  if (user) {
+    USER_ID = user.uid;
+    markupLibraryRender(USER_ID);
+  } else {
+    alert('You must be loginned for using this function!');
   }
-  console.log('NOT LOGIN!!!!');
+});
+
+let FILTER = 'watched';
+
+const listFilms = document.querySelector('.list-films');
+const watched = document.querySelector('[data-watched-btn]');
+const queue = document.querySelector('[data-queue-btn]');
+watched.addEventListener('click', onClickFilterChange);
+queue.addEventListener('click', onClickFilterChange);
+
+function onClickFilterChange(e) {
+  if (e.target.hasAttribute('data-watched-btn')) {
+    FILTER = 'watched';
+    markupLibraryRender(USER_ID);
+    return;
+  }
+  FILTER = 'queue';
+  markupLibraryRender(USER_ID);
 }
 
 function markupLibraryRender(uid) {
-  const savedMovies = localStorage.getItem('genres');
-  const parsedMovies = JSON.parse(savedGenres);
+  const savedMovies = localStorage.getItem(uid);
+  const parsedMovies = JSON.parse(savedMovies);
 
-  console.log(parsedMovies);
+  const array = parsedMovies[FILTER];
 
   const markup = array
     .map(item => {
       return `<li class="link list-films_card" data-id='${item.id}'>
         <a class="link list-films_card-info">
-            <img
-                class="list-films_card-info_card-film"
-                src=https://image.tmdb.org/t/p/original/${item.poster_path}
-                alt="movie poster"
-            />
+                <div class="card-image__wrapper">
+                    <img
+                    class="list-films_card-info_card-film"
+                    src=https://image.tmdb.org/t/p/original/${item.poster_path}
+                    alt="${item.original_title}"
+                />
+                </div>
             <div class="list-films_card-info-footer">
             <h2 class="list-films_card-info-footer-name-film">${item.title.slice(
               0,
@@ -46,6 +66,6 @@ function markupLibraryRender(uid) {
         </li>`;
     })
     .join('');
-
+  listFilms.innerHTML = '';
   listFilms.insertAdjacentHTML('afterbegin', markup);
 }
