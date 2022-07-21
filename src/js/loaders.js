8695e2b5421aa0aaaa29a901f08228a201f454a8
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-21 22:41:30 +0300

    done loaders

--- src/js/loaders.js
+++ src/js/loaders.js
@@ -7,3 +7,20 @@ export function disableSpinnerOn(btn, spin) {
   btn.disabled = false;
   spin.classList.add('submit-spinner_hide');
 }
+
+
+export function enableSpinnerSearch() {
+  const spinner = document.querySelector('#spinnersearch');
+  const icon = document.querySelector('.search__button-icon');
+
+  spinner.classList.remove('submit-spinner_hide');
+  icon.classList.add('submit-spinner_hide');
+}
+
+export function disableSpinnerSearch() {
+  const spinner = document.querySelector('#spinnersearch');
+  const icon = document.querySelector('.search__button-icon');
+
+  spinner.classList.add('submit-spinner_hide');
+  icon.classList.remove('submit-spinner_hide');
+}
