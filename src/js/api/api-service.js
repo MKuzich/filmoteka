9a86369d518d53dd362298c3591e86ce119ef11c
IrynaZhuk <irynazh94@gmail.com>
2022-07-21 16:32:38 +0300

    lang-added-to-api-search

--- src/js/api/api-service.js
+++ src/js/api/api-service.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 const API_KEY = '0d2fc6d2597c3f501e7437cf40d08b03';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const SEARCH_URL = `${BASE_URL}/search/movie`;
+const lang = localStorage.getItem('localLang');
 
 
     export async function fetchMovieSearcher(text, page) {
         try {
           const { data } = await axios.get(
-            `${SEARCH_URL}?api_key=${API_KEY}&query=${text}&page=${page}`,
+            `${SEARCH_URL}?api_key=${API_KEY}&query=${text}&language=${lang}&page=${page}`,
           );
     
           return data;
