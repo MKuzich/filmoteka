ca8f12a74f4bced97e4a3085ad99182a9b38d2c0
Mykhailo Kuzich <timmkedth@gmail.com>
2022-07-21 22:42:33 +0300

    genres-fetch-api with selected linguage

--- src/js/api/api-service-genres.js
+++ src/js/api/api-service-genres.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 const API_KEY = '0d2fc6d2597c3f501e7437cf40d08b03';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const SEARCH_URL = `${BASE_URL}/genre/movie/list`;
+let lang = '';
 
 export async function fetchMovieGenres() {
   try {
+    lang = localStorage.getItem('localLang');
     const { data } = await axios.get(
-      `${SEARCH_URL}?api_key=${API_KEY}&language=en-US`
+      `${SEARCH_URL}?api_key=${API_KEY}&language=${lang}`
     );
     localStorage.setItem('genres', JSON.stringify(''));
     localStorage.setItem('genres', JSON.stringify(data.genres));
