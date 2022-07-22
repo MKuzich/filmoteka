11c312d1ff48bd194e79b45619af7be6a9550167
Nedzhat <nexuak@gmail.com>
2022-07-22 21:45:41 +0300

    fix bug for 1-7 pages

---
+++ src/js/pagination-library.js
@@ -0,0 +1,11 @@
+// import { currentPage } from './pagination';
+
+// 1 - 0 - 9
+// 2 - 10 - 18
+// 3 - 19 - 27
+// 4 - 28 - 36
+// 5 - 37 - 45
+
+// if (array.length > 9) {
+//   console.log(array.slice((currentPage - 1) * 9 + 1, currentPage * 9));
+// }
