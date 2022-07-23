c589de01e25cfe548498742dc06868f67142a6f9
Vitaliy Dianov <vitalik.dianv@gmail.com>
2022-07-23 17:56:55 +0300

    Added dark theme

--- src/js/pagination-markup.js
+++ src/js/pagination-markup.js
@@ -3,13 +3,13 @@ import { currentPage } from './pagination';
 const galleryRef = document.querySelector('.library-cards-film');
 
 const btnLeft = `<button class='btn-pagination-arrow' data-action='flip-left'><svg class='svg-pagination-btn' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12.6666 8H3.33325" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M7.99992 12.6667L3.33325 8.00004L7.99992 3.33337" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12.6666 8H3.33325" stroke="inherit" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7.99992 12.6667L3.33325 8.00004L7.99992 3.33337" stroke="inherit" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 </button>`;
 const btnRight = `<button class='btn-pagination-arrow' data-action='flip-right'><svg class='svg-pagination-btn' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M3.33341 8H12.6667" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M8.00008 12.6667L12.6667 8.00004L8.00008 3.33337" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M3.33341 8H12.6667" stroke="inherit" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.00008 12.6667L12.6667 8.00004L8.00008 3.33337" stroke="inherit" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 </button>`;
 
